8c86cf39e285926d6650859c413490d38894f171
alkcode <alek_1604@comunidad.unam.mx>
2022-03-11 14:09:51 -0600

    Vista de login y dashboard

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 	
 	@GetMapping("/")
 	public String mostrarHome() {
-		return "home";
+		return "blank";
 	}
 
 }
